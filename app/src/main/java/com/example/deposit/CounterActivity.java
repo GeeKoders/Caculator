c2ebecf15500631e8ecd8d49b2b7a493bd570750
Geekoders <iloveitone@gmail.com>
2016-04-04 14:41:19 +0800

    2016.04.04 Paul modified: Caculator finish

--- app/src/main/java/com/example/deposit/CounterActivity.java
+++ app/src/main/java/com/example/deposit/CounterActivity.java
@@ -1,12 +1,12 @@
 package com.example.deposit;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class CounterActivity extends AppCompatActivity {
+public class CounterActivity extends Activity {
 
 
     private TextView calResult;
